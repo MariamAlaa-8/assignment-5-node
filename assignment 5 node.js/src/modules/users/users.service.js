3ad1dc8882545edba8416571c8d8a70dc97d7a48
Mariam alaa <mariamalaa203030@gmail.com>
2026-08-21 22:19:23 +0200

    t1

--- assignment 5 node.js/src/modules/users/users.service.js
+++ assignment 5 node.js/src/modules/users/users.service.js
@@ -1,26 +1,16 @@
-import { User } from "../../DB/index.js";
-import { Op } from "sequelize";
-
+import { User } from "../../DB/index.js"
+import { Op } from "sequelize"
 //a2
-
-
 export const upsertUserService = async (id, data) => {
     const { name, email, password, role } = data;
-
     const user = await User.findByPk(id);
-
-    let isNew = false;  
-
+    let isNew = false  
     if (user) {
-       
         await user.update(
             { name, email, password, role },
             { validate: false }
         );
-        return { user, isNew: false };
-    }
-
-  
+        return { user, isNew: false };}
     isNew = true;
     const newUser = await User.create(
         { id, name, email, password, role },
@@ -29,7 +19,6 @@ export const upsertUserService = async (id, data) => {
     return { user: newUser, isNew: true };
 };
 //a3
-
 export const getUserByEmailService = async (email) => {
     const user = await User.findOne({
         where: { email }
@@ -41,9 +30,7 @@ export const getUserByEmailService = async (email) => {
         throw error;
     }
 
-    return user;
-};
-
+    return user;};
 //a4
 export const getUserByIdService = async (id) => {
     const user = await User.findByPk(id, {
@@ -51,7 +38,6 @@ export const getUserByIdService = async (id) => {
             exclude: ["role"]
         }
     });
-
     if (!user) {
         const error = new Error("no user found");
         error.cause = { status: 404 };
